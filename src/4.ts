4578ad97c9d1cef0260bb8da0bbd282df43e3158
ImaLorikI <pasha.bandrivskiy@gmail.com>
2024-03-12 00:05:28 +0200

    try1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,46 @@
+class Key {
+  private signature = Math.random();
+  public getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(mainKey: Key) {
+    super();
+    this.key = mainKey;
+  }
+  openDoor(mainKey: Key): void {
+    if (mainKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
+console.log(house.tenants.length);
 
 export {};
